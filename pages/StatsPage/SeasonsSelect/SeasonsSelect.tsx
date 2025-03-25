2613a4e3639e00a644a034e71527ab7e8073208f
florianerwerth <florian.erwerth@ambient.digital>
2025-03-25 16:41:23 +0100

    add biome config

--- pages/StatsPage/SeasonsSelect/SeasonsSelect.tsx
+++ pages/StatsPage/SeasonsSelect/SeasonsSelect.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {ActivityIndicator, ScrollView} from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import { useAllSeasonsQuery } from '~/api/seasons';
 import { SelectableButtonGroup } from '~/components/SelectableButtonGroup/SelectableButtonGroup';
 import { Text } from '~/components/Text';
 import { useStatsStore } from '~/store/stats';
-import {BottomSheetScrollView} from "@gorhom/bottom-sheet";
-import {useSafeAreaInsets} from "react-native-safe-area-context";
+import { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type SeasonsSelectSheetContentProps = {
   onSelectSeason: () => void;
@@ -14,7 +14,7 @@ type SeasonsSelectSheetContentProps = {
 export const SeasonsSelectSheetContent = ({ onSelectSeason }: SeasonsSelectSheetContentProps) => {
   const { data, isLoading } = useAllSeasonsQuery();
   const { season, setSeason } = useStatsStore();
-  const { bottom } = useSafeAreaInsets()
+  const { bottom } = useSafeAreaInsets();
   if (isLoading) {
     return <ActivityIndicator size="large" />;
   }
@@ -35,14 +35,14 @@ export const SeasonsSelectSheetContent = ({ onSelectSeason }: SeasonsSelectSheet
   };
 
   return (
-    <BottomSheetScrollView contentContainerStyle={{paddingBottom: "10%"}}>
+    <BottomSheetScrollView contentContainerStyle={{ paddingBottom: '10%' }}>
       <SelectableButtonGroup
-      defaultValue="lifetime"
-      onSelectOption={handleSetSeason}
-      value={season.name}
-      maxItemsInRow={2}
-      options={data.map((season) => season.name)}
-    />
+        defaultValue="lifetime"
+        onSelectOption={handleSetSeason}
+        value={season.name}
+        maxItemsInRow={2}
+        options={data.map((season) => season.name)}
+      />
     </BottomSheetScrollView>
   );
 };
